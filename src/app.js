dcbcd9592a9efb1a691c02e0e04bc146f04332e5
Fran-JF <franciscouzcategui2021@gmail.com>
2023-06-26 05:22:00 -0400

    Instalacion de dependencias y creacion de carpetas en el server

---
+++ src/app.js
@@ -0,0 +1,7 @@
+// Importamos y ejecutamos el express
+import express from "express";
+const app = express();
+
+
+// Exportamos la variable app
+export default app
---
+++ src/index.js
@@ -0,0 +1,5 @@
+//Inportamos app desde el archivo app
+import app from "./app.js";
+
+app.listen(3000)
+console.log("server en puerto", 3000);
